1016a8534db7cd2775d8d0eb712db0abe9c6298a
V.Yermakovich <erviktoria19@gmail.com>
2021-07-06 17:07:24 +0300

    pom.xml

--- src/test/java/steam/pages/AgePage.java
+++ src/test/java/steam/pages/AgePage.java
@@ -19,7 +19,7 @@ public class AgePage extends BaseSteamPage {
     }
 
     public static boolean waitForPageToLoad() {
-        Label lblAgePage =new Label(By.xpath(String.format(pageLocator,getProperty("age_check"))), "Age page");
+        Label lblAgePage =new Label(By.xpath(String.format(pageLocator, prop.getProperty("age_check"))), "Age page");
         if (!lblAgePage.waitForIsElementPresent()) {
             return false;
         } else {
@@ -29,7 +29,7 @@ public class AgePage extends BaseSteamPage {
 
     public BasePage checkAge(String interYear) {
         comboBoxYear.selectComboBox(interYear);
-        Button btnViewPage = new Button(By.xpath(String.format(btnViewPageTemplate, getProperty("view_page_key"))));
+        Button btnViewPage = new Button(By.xpath(String.format(btnViewPageTemplate, prop.getProperty("view_page_key"))));
         btnViewPage.clickAndWait();
         return new GamePage();
     }
--- src/test/java/steam/pages/MainMenu.java
+++ src/test/java/steam/pages/MainMenu.java
@@ -1,18 +1,20 @@
 package steam.pages;
 
+import framework.PropertyReader;
 import framework.elements.Label;
 import org.openqa.selenium.By;
 
 import static framework.PropertyReader.getProperty;
 
-public class MainMenu{
+public class MainMenu {
+    PropertyReader prop = new PropertyReader();
     private static String lblMenuSectionTemplate = "//a[@class='pulldown_desktop'][contains(text(),'%s')]";
     private static String lblSubSectionTemplate = "//div[contains(@class,'popup_block_new flyout_tab_flyout responsive_slidedown')]//a[contains(text(),'%s')]";
 
     public void navigateSection(String sectionName, String navSection) {
-        Label lblMenu = new Label(By.xpath(String.format(lblMenuSectionTemplate, getProperty(sectionName))));
+        Label lblMenu = new Label(By.xpath(String.format(lblMenuSectionTemplate, prop.getProperty(sectionName))));
         lblMenu.click();
-        Label lblSection = new Label(By.xpath(String.format(lblSubSectionTemplate, getProperty(navSection))));
+        Label lblSection = new Label(By.xpath(String.format(lblSubSectionTemplate, prop.getProperty(navSection))));
         lblSection.clickAndWait();
     }
 }
