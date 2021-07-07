5d67256959b1dbfc1b03e3a9b9480f2427a93484
V.Yermakovich <erviktoria19@gmail.com>
2021-07-07 14:46:58 +0300

    Fixed language

--- src/test/java/steam/pages/AgePage.java
+++ src/test/java/steam/pages/AgePage.java
@@ -21,9 +21,9 @@ public class AgePage extends BaseSteamPage {
     public static boolean waitForPageToLoad() {
         Label lblAgePage =new Label(By.xpath(String.format(pageLocator, getProperty("age_check"))), "Age page");
         if (!lblAgePage.waitForIsElementPresent()) {
-            return false;
-        } else {
             return true;
+        } else {
+            return false;
         }
     }
 
