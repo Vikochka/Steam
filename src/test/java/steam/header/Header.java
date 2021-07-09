eb8ae8901027ccd2c2c3bec587aee9bf726286c7
V.Yermakovich <erviktoria19@gmail.com>
2021-07-09 10:04:53 +0300

    Added        lblpopup.waitForIsElementPresent();

--- src/test/java/steam/header/Header.java
+++ src/test/java/steam/header/Header.java
@@ -12,13 +12,15 @@ public class Header {
     private Label lblLanguage = new Label(By.xpath("//span[@id='language_pulldown']"));
     private Button btnInstallSteam = new Button(By.xpath("//a[@class='header_installsteam_btn_content']"));
     private static String btnLanguage = "//a[@class='popup_menu_item tight'][contains(text(),'%s')]";
+    private static Label lblpopup= new Label(By.xpath("//div[@id='language_dropdown']"));
 
     public void selectLanguage(String selectLanguage) {
         System.out.println("1");
         lblLanguage.clickViaJS();
         System.out.println("2");
+        lblpopup.waitForIsElementPresent();
+        System.out.println("i see you");
         TextBox currentLanguage = new TextBox(By.xpath(String.format(btnLanguage, selectLanguage)));
-        currentLanguage.isDisplayed();
         System.out.println("6");
         if (currentLanguage.isDisplayed()) {
             System.out.println("3");
